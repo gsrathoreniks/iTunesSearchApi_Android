e66cb998645ae327f8af4cf10ca66efb69aea5cb
Gajendra Singh Rahtore <niks.gajsa@gmail.com>
2017-11-15 03:21:05 +0530

    Initial commit

---
+++ app/src/main/java/gajendrasinghrathore/itunessongslist/utilities/Constants.java
@@ -0,0 +1,9 @@
+package gajendrasinghrathore.itunessongslist.utilities;
+
+/**
+ * Created by gajendrasinghrathore on 14/11/17.
+ */
+
+public class Constants {
+    public static String baseURL = "https://itunes.apple.com/search?term=Michael+Jackson";
+}
